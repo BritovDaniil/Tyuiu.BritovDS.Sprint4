81e2b48dfbd1f694a9a16e3f7f0054ae467e2a1f
user <user@WIN-FJ5S0KDD7IV>
2025-11-24 20:00:01 +0300

    Task2

---
+++ Tyuiu.BritovDS.Sprint4.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BritovDS.Sprint4.Task2.V6.Lib;
+namespace Tyuiu.BritovDS.Sprint4.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 8 };
+
+            int res = ds.Calculate(numsArray);
+            int wait = 1594323;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
